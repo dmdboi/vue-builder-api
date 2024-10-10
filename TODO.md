0162d1fd7670b07de00fc01c43119e82bab0f291
dmdboi <padfootdiamond@gmail.com>
2024-10-10 20:29:03 +0100

    feat: allow resolving components in Pages/Templates

--- TODO.md
+++ TODO.md
@@ -4,7 +4,8 @@
 - [X] Template CRUD endpoints
 - [X] Page model
 - [ ] Insert Page into Template
-    - [-] Allow resolving components in Templates
+    - [X] Allow resolving components in Templates
+    - [X] Allow resolving components when returning a Page
 - [ ] Support for Users and Teams
 - [ ] Site model to store site-wide settings (e.g. site name, description, meta tags)
 - [ ] File upload support (local, R2 or S3 storage)
--- src/controllers/templates/get.ts
+++ src/controllers/templates/get.ts
@@ -2,16 +2,30 @@ import { Request, Response } from "express";
 
 import { renderPageHTML } from "../../libs/renderer";
 import Template from "../../models/Template";
+import { getComponentsInTemplate, replaceComponentRefs } from "../../libs/template";
+import Component from "../../models/Component";
 
 async function get(req: Request, res: Response) {
   const { id } = req.params;
 
   // Find the content in MongoDB
-  let data = await Template.findOne({ id: id });
+  let template = await Template.findOne({ id: id });
+
+  const results = await getComponentsInTemplate(template.content);
+
+  if (results.message === "Error") {
+    res.status(404).json(results);
+  }
+
+  if (results.components!.length > 0) {
+    // Find all components in Database by componentRef
+    const dbComponents = await Component.find({ id: { $in: results.components.map((component: any) => component.componentRef) } });
+    template.content = replaceComponentRefs(template.content, dbComponents);
+  }
 
   res.status(200).json({
     message: "Success",
-    data,
+    data: template,
   });
 }
 
