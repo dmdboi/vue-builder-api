99526fed53bfb04700acdba4bffd01c0d678a409
dmdboi <padfootdiamond@gmail.com>
2024-10-11 19:57:10 +0100

    feat: remove repeatable objects

--- src/controllers/components/store.ts
+++ src/controllers/components/store.ts
@@ -5,23 +5,11 @@ import { buildRepeatableContent, findRepeatableContent, insertRepeatableContent 
 import Component from "../../models/Component";
 
 async function store(req: Request, res: Response) {
-  const results = await findRepeatableContent(req.body.content);
-
-  const buildableResult = await Promise.all(results.map(async result => await buildRepeatableContent(result, req.body.data.menu)));
-
-  // Flatten the array
-  const flattened = buildableResult.flat(Infinity);
-
-  // For each repeatable content object, insert the repeatable content into the content array
-  const outputResult = await Promise.all(flattened.map(async result => await insertRepeatableContent(req.body.content, result.content, result.repeatable as string)));
-  const flatOutputResult = outputResult.flat(Infinity);
-
   // Build component content
   const component = {
     ...req.body,
     type: "component",
     id: ulid(),
-    content: flatOutputResult,
   };
 
   // Save the component to the database
@@ -34,25 +22,7 @@ async function store(req: Request, res: Response) {
 }
 
 async function update(req: Request, res: Response) {
-  let payload = req.body;
-
-  const results = await findRepeatableContent(req.body.content);
-
-  if (results.length > 0) {
-    const buildableResult = await Promise.all(results.map(async result => await buildRepeatableContent(result, req.body.data.menu)));
-
-    // Flatten the array
-    const flattened = buildableResult.flat(Infinity);
-
-    // For each repeatable content object, insert the repeatable content into the content array
-    const outputResult = await Promise.all(flattened.map(async result => await insertRepeatableContent(req.body.content, result.content, result.repeatable as string)));
-    const flatOutputResult = outputResult.flat(Infinity);
-
-    payload = {
-      ...req.body,
-      content: flatOutputResult,
-    };
-  }
+  const payload = req.body;
 
   await Component.updateOne(
     { id: req.params.id },
